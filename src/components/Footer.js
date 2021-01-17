0513e6ecf17db16c3f1be6883dad272d26dabbe5
Jose Manuel <castellanogonzalezjosemanuel@gmail.com>
2021-01-17 12:39:21 +0100

    agregado al dominio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-  return (
-    <footer>Carmen, Jose Manuel & Ismael<br></br>SEI España 2020
-    </footer>
-  );
+  return <footer></footer>;
 };
 
 export default Footer;
