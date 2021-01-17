99ab89817d2cbbd5a07e88574e35693dff2915b8
Jose Manuel <castellanogonzalezjosemanuel@gmail.com>
2021-01-17 12:54:27 +0100

    build in domain

---
+++ build/precache-manifest.4b03aa52f30f98149cdb9257255bd15b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b960ba082c02f564ff74c1036dcaeb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1849a552e4756b7d323",
+    "url": "/static/css/main.e60e204a.chunk.css"
+  },
+  {
+    "revision": "a66aeab4561c66dbffd7",
+    "url": "/static/js/2.60ddde6c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.60ddde6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1849a552e4756b7d323",
+    "url": "/static/js/main.2733b4db.chunk.js"
+  },
+  {
+    "revision": "47042b47e27af43cbf50",
+    "url": "/static/js/runtime-main.d331308a.js"
+  },
+  {
+    "revision": "20d40b9b1d0729a224a21c4799d0bd26",
+    "url": "/static/media/a11y.20d40b9b.svg"
+  },
+  {
+    "revision": "a14738bb547ec0e77504488739eed02e",
+    "url": "/static/media/blue-flag.a14738bb.svg"
+  },
+  {
+    "revision": "acabe3f19dc00ab4f97f4ff68a72ff38",
+    "url": "/static/media/playa.acabe3f1.jpg"
+  }
+]);
